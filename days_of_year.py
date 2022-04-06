d4d52ce05f4b92549f044fcf0ac62f9b7fe7b0b7
Faisal Al-Rayes <faisalalrayyess@icloud.com>
2022-04-06 17:35:29 +0200

    Update

--- days_of_year.py
+++ days_of_year.py
@@ -15,26 +15,38 @@ def day_number(year: int, month: int, day: int) -> int:
         12: 31
     }
 
-    assert 0 < day <= Month_days[month] and year > 0, 'invalid day, month, or year input.'
+    if year < 0:
+        raise ValueError("Inapproperiate year value.")
 
-    number_of_days_passed = 0
+    if month not in Month_days.keys():
+        raise KeyError("Inapproperiate month value.")
+
+    if day not in range(1, Month_days[month]+1):
+        raise ValueError("Inapproperiate day value.")
+
+    #Condition for the years that have a 29th day in February.
     if year % 4 == 0:
         Month_days[2] = 29
 
+    #The code that 
+    number_of_days_passed = 0
     for k, v in Month_days.items():
         if k < month:
             number_of_days_passed = number_of_days_passed + v
         elif k == month:
             number_of_days_passed = number_of_days_passed + day
             print(number_of_days_passed)
+            return number_of_days_passed
             break
 
 
-# day_number(2024, "jan", 2)
-# day_number(2024,3,1)
-# day_number(2021,3,1)
-# day_number(2021,80,2)
-# day_number(2021,3,200)
-# day_number(-200,3,2)
+# day_number(-200,3,2)                  # Checking the ValueError being raised as per line 19
+# day_number(2024, "jan", 2)            # Checking the KeyError being raised as per line 22
+# day_number(2021,80,2)                 # Checking the KeyError being raised as per line 22
+# day_number(2021,3,200)                # Checking the ValueError being raised as per line 25
+# day_number(2024,3,1)                  # Checking the inclusion of Feb 29
+# day_number(2021,3,1)                  # Checking the exclusion of Feb 29
+
+
 
 
